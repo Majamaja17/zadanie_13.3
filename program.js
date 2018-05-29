5eaa200ce4321b5be9a978f5d9c99c1294a56185
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-29 21:16:04 +0200

    Add gitignore, package and js files

---
+++ program.js
@@ -0,0 +1,18 @@
+console.log(process.versions);
+console.log(process.env);
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    switch (input !== null) {
+        var instruction = input.toString().trim();
+
+        case (instruction === '/exit')
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        case {
+            process.stderr.write('Wrong instruction!');
+        }
+    }
+});
