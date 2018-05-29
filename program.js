b16f0e6f65c020862d7e8dde41ae46f036ee3d21
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-29 22:16:50 +0200

    Change js file

--- program.js
+++ program.js
@@ -1,7 +1,6 @@
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
@@ -10,6 +9,7 @@ process.stdin.on('readable', function() {
 		    case '/exit':
 		        process.stdout.write('Quitting app!\n');
 		        process.exit();
+		        break;
 
 		     default:
 		     process.stderr.write('Wrong instruction!');
